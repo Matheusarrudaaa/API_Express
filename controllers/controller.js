10d6df2b8fb8eaf10d597420c61a99e07530699d
Matheus Henrique De Arruda <92222123+Matheusarrudaaa@users.noreply.github.com>
2022-12-08 14:46:05 -0300

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 exports.pong = (req, res) => {
 
     res.send(`
-        <form action="/ping/msg" method="POST">
+        <form action="/ping" method="POST">
         <label>Mensagem:</label>
         <input type="text" name="Mensagem">
         <button>Enviar</button> 
@@ -14,6 +14,6 @@ exports.pong_msg = (req, res) => {
     console.log(req.body)
 
     const json = JSON.stringify(req.body);
-    fs.writeFile("./teste.json", json, { flag: 'w' });
+    fs.writeFile("./msg.json", json, { flag: 'w' });
     res.send(`<h1>Mensagem recebida e salva!!! </h1>`)
 }
