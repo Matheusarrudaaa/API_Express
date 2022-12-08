b210b284e53ae004b7ae46c765746c9deda6c55c
Matheus Henrique De Arruda <92222123+Matheusarrudaaa@users.noreply.github.com>
2022-12-08 14:45:27 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 controller = require('./controllers/controller')
 
 router.get('/ping', controller.pong)
-router.post('/ping/msg', controller.pong_msg)
+router.post('/ping', controller.pong_msg)
 
 
 module.exports = router;
